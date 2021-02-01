b547e6160994eef6de88d988166f1e008b9e3161
root <root@book>
2021-02-01 15:09:46 +0500

    Updates

---
+++ precache-manifest.12f9bc4a5abd0de80cf44c0cca2e21cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e605c92501b76dcc4ad3c1435c9bb212",
+    "url": "/bird-oracle-ui/index.html"
+  },
+  {
+    "revision": "55bf59b1fe480064c77e",
+    "url": "/bird-oracle-ui/static/css/main.1350eab6.chunk.css"
+  },
+  {
+    "revision": "7c028d4df52c3556700e",
+    "url": "/bird-oracle-ui/static/js/2.b27236a1.chunk.js"
+  },
+  {
+    "revision": "ffd01bdc2f31e32fc2fd27bdae5dcef0",
+    "url": "/bird-oracle-ui/static/js/2.b27236a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55bf59b1fe480064c77e",
+    "url": "/bird-oracle-ui/static/js/main.18c618b7.chunk.js"
+  },
+  {
+    "revision": "439bb781b674484b3b8d",
+    "url": "/bird-oracle-ui/static/js/runtime-main.58827d90.js"
+  },
+  {
+    "revision": "7b9104d977ff623ba83cec9827f010bb",
+    "url": "/bird-oracle-ui/static/media/bird-cells.7b9104d9.svg"
+  },
+  {
+    "revision": "d4a93e685d44382ee267d81269adefc9",
+    "url": "/bird-oracle-ui/static/media/bird_logo.d4a93e68.png"
+  }
+]);
